55d037fb5b144e7d1e8ff6a5e7dbf92a483a433a
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-10 16:33:03 +0700

    update

--- rush_clean.c
+++ rush_clean.c
@@ -16,28 +16,28 @@
 void	ft_putchar(char c)
 void	rush(int x, int y)
 {
-	int c;
-       	int r;
+int c;
+int r;
 
-    if (x == 0 || y == 0)
-        return;
-    r = 1;
-    while (r <= y)
+if (x == 0 || y == 0)
+return;
+r = 1;
+while (r <= y)
+{
+c = 1;
+while (c <= x)
     {
-        c = 1;
-        while (c <= x)
-        {
-            if ((c == 1 && r == 1) || (c == 1 && r == y))
-                ft_putchar(head);
-            else if ((c == x && r == 1) || (c == x && r == y))
-                ft_putchar(tail);
-            else if (r == 1 || c == 1 || r == y || c == x)
-                ft_putchar(between);
-            else
-                ft_putchar(' ');
-            c++;
-        }
-        ft_putchar('\n');
-        r++;
-    }
+if ((c == 1 && r == 1) || (c == 1 && r == y))
+ft_putchar(head);
+else if ((c == x && r == 1) || (c == x && r == y))
+ft_putchar(tail);
+else if (r == 1 || c == 1 || r == y || c == x)
+ft_putchar(between);
+else
+ft_putchar(' ');
+c++;
+}
+ft_putchar('\n');
+r++;
+}
 }
