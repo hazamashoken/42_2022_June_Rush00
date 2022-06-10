db0469a3682584ccd55e50796fd82a56e536cda6
parrot <parrot@localhost.localdomain>
2022-06-10 17:03:49 +0700

    bruh

---
+++ rush00/rush04/main.c
@@ -0,0 +1,7 @@
+#include "rush04.c"
+
+int main ()
+{
+   rush(5, 5);
+   return (0);
+}
---
+++ rush00/rush04/rush04.c
@@ -0,0 +1,32 @@
+#include "ft_putchar.c"
+
+void    ft_print(int a, int b, int w, int h)
+{
+  if ((a == 0 && b == 0) || (a == h - 1 && b == w - 1 && a != h - 1))
+    ft_putchar(65); // A
+  else if ((a == 0 && b == w - 1) || (a == h - 1 && b == 0))
+    ft_putchar(67); // C
+  else if ((a == 0 || a == h - 1) || (b == 0 || b == w - 1))
+    ft_putchar(66); // B
+  else
+    ft_putchar(32); // [space]
+}
+
+void    rush(int w, int h)
+{
+  int a;
+  int b;
+
+  a = 0;
+  while (a < h)
+  {
+    b = 0;
+    while (b < w)
+    {
+      ft_print(a, b, w, h);
+      b++;
+    }
+    ft_putchar(10); // \n
+    a++;
+  }
+}
