faff204ba372986a6f820fb35dae0f8e32535885
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-10 13:50:23 +0700

    sc

---
+++ rush0x.c
@@ -0,0 +1,18 @@
+#define UPLEFT 'A'
+#define UPRIGHT 'C'
+#define DOWNLEFT 'A'
+#define DOWNRIGHT 'C'
+
+void	ft_putchar(char c);
+
+void    rush(int horizon, int vertical)
+{
+    int _horizon;
+    int _vertical;
+    int count;
+
+    if (horizon = 0 || vertical = 0)
+        return ;
+
+
+}
