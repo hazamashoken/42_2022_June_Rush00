df36fc6f53303c1d9f562fe26d12e9624ab60c30
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-06-10 14:08:48 +0700

    Complete rush00 (See description)
    I know it's not the best code, but it works though. Further testing is required to check the program's robust.
    
    Also, the norminette doesn't allow you to include any .c file. So, in order to test, uncomment the #include.

---
+++ rush00/rush00/main.c
@@ -0,0 +1,7 @@
+// #include "rush01.c"
+
+int main()
+{
+    rush(123, 42);
+    return (0);
+}
---
+++ rush00/rush00/rush01.c
@@ -0,0 +1,91 @@
+// #include "ft_putchar.c"
+
+void    print_top(int column)
+{
+    int i;
+
+    i = 0;
+    while (i < column)
+    {
+        if (i == 0)
+        {
+            ft_putchar('/');
+        }
+        else if (i == column - 1)
+        {
+            ft_putchar('\\');
+        }
+        else
+        {
+            ft_putchar('*');
+        }
+        i++;
+    }
+    ft_putchar('\n');
+}
+
+void    print_middle(int column, int row)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (i < row - 2)
+    {
+        j = 0;
+        while (j < column)
+        {
+            if ((j == 0) || (j == column - 1))
+            {
+                ft_putchar('*');
+            }
+            else
+            {
+                ft_putchar(' ');
+            }
+            j++;
+        }
+        ft_putchar('\n');
+        i++;
+    }
+}
+
+void    print_bottom(int column)
+{
+    int i;
+
+    i = 0;
+    while (i < column)
+    {
+        if (i == 0)
+        {
+            ft_putchar('\\');
+        }
+        else if (i == column - 1)
+        {
+            ft_putchar('/');
+        }
+        else
+        {
+            ft_putchar('*');
+        }
+        i++;
+    }
+    ft_putchar('\n');
+}
+
+void    rush(int column, int row)
+{
+    if (row > 0)
+    {
+        print_top(column);
+    }
+    if (row > 1)
+    {
+        print_middle(column, row);
+    }
+    if (row > 2)
+    {
+        print_bottom(column);
+    }
+}
