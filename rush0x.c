4ac47f039b8094150e5b00377264b8056fe32dd6
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-10 14:17:34 +0700

    commit

--- rush0x.c
+++ rush0x.c
@@ -1,18 +1,32 @@
-#define UPLEFT 'A'
-#define UPRIGHT 'C'
-#define DOWNLEFT 'A'
-#define DOWNRIGHT 'C'
-
 void	ft_putchar(char c);
 
-void    rush(int horizon, int vertical)
+void    rush(int x, int y)
 {
-    int _horizon;
-    int _vertical;
+    int i;
+    int j;
     int count;
 
-    if (horizon = 0 || vertical = 0)
+    count = 1;
+
+    if (x = 0 || y = 0)
         return ;
-
-
+    j = 1;
+    while (j <= y)
+    {
+        i = 1;
+        while (i <= x)
+        {
+            if ((i == 1 && j == 1) || (i == 1 && j == y))
+                ft_putchar('A')
+            else if ((i == x && j == 1) || (i == x && j == y))
+                ft_putchar('C')
+            else if ((i > x && j == 1) || (i > x && j == y))
+                ft_putchar('B')
+            else
+                ft_putchar(' ')
+            i += 1;
+        }
+        ft_putchar('/n')
+        j += 1;
+    }
 }
