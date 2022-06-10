330ea2e5f01648a6209a5501a255866d7e5d7037
Ruangsak Patomwong <rpatomwo@z2t7c3.42bangkok.com>
2022-06-10 17:26:24 +0700

    aaa

--- newrush.c
+++ newrush.c
@@ -1,16 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush_clean.c                                       :+:      :+:    :+:   */
+/*   newrush.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpatomwo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/10 16:18:50 by rpatomwo          #+#    #+#             */
-/*   Updated: 2022/06/10 17:05:14 by rpatomwo         ###   ########.fr       */
+/*   Created: 2022/06/10 17:06:36 by rpatomwo          #+#    #+#             */
+/*   Updated: 2022/06/10 17:25:33 by rpatomwo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c)
+void	ft_putchar(char c);
+
 void	rush(int x, int y)
 {
 	int	c;
@@ -25,12 +26,12 @@ void	rush(int x, int y)
 			if ((c == 1 && r == 1) || (c == 1 && r == y))
 				ft_putchar('A');
 			else if ((c == x && r == 1) || (c == x && r == y))
-				ft_putchar('B');
-			else if (r == 1 || c == 1 || r == y || c == x)
 				ft_putchar('C');
+			else if (r == 1 || c == 1 || r == y || c == x)
+				ft_putchar('B');
 			else
 				ft_putchar(' ');
-				c++;
+			c++;
 		}
 		ft_putchar('\n');
 		r++;
