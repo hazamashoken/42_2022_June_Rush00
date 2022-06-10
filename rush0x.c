4b5b03ec8ee2362154cdaaa2504971441e8d97d0
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-10 15:11:03 +0700

    update viarable name

--- rush0x.c
+++ rush0x.c
@@ -2,31 +2,31 @@ void	ft_putchar(char c);
 
 void    rush(int x, int y)
 {
-    int i;
-    int j;
+    int count_horizon;
+    int count_vertical;
     int count;
 
     count = 1;
 
     if (x == 0 || y == 0)
         return ;
-    j = 1;
-    while (j <= y)
+    count_vertical= 1;
+    while (count_vertical<= y)
     {
-        i = 1;
-        while (i <= x)
+        count_horizon = 1;
+        while (count_horizon <= x)
         {
-            if ((i == 1 && j == 1) || (i == 1 && j == y))
+            if ((count_horizon == 1 && count_vertical== 1) || (count_horizon == 1 && count_vertical== y))
                 ft_putchar('A');
-            else if ((i == x && j == 1) || (i == x && j == y))
+            else if ((count_horizon == x && count_vertical== 1) || (count_horizon == x && count_vertical== y))
                 ft_putchar('C');
-            else if ((i > 1 && j == 1) || (i > 1 && j == y) || (i = 1 && j > 1) || (i = x && j > 1))
+            else if ((count_horizon > 1 && count_vertical== 1) || (count_horizon > 1 && count_vertical== y) || (count_horizon = 1 && count_vertical> 1) || (count_horizon = x && count_vertical> 1))
                 ft_putchar('B');
             else
                 ft_putchar(' ');
-            i += 1;
+            count_horizon += 1;
         }
         ft_putchar('\n');
-        j += 1;
+        count_vertical+= 1;
     }
 }
